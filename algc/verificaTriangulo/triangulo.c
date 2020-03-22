1639d6e8ec6e4a9ad459500eb1fc1d695ca59dcc
saulo <saulo@pop-os.localdomain>
2020-03-22 00:01:01 -0300

    Verificador de triangulos

---
+++ algc/verificaTriangulo/triangulo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a, b, c;
+
+    printf("Insira o lado A do triângulo:\n");
+    scanf("%d", &a);
+    
+    printf("Insira o lado B do triângulo:\n");
+    scanf("%d", &b);
+    
+    printf("Insira o lado C do triângulo:\n");
+    scanf("%d", &c);
+
+    if(a + b > c && a + c > b && b + c > a){
+        printf("Os valores inseridos são válidos para um triângulo!\n");
+    }else{
+        printf("Os valores inseridos não são válidos!\n");
+    }
+    if(a == b && a == c){
+        printf("EQULILÁTERO!\n");
+    }
+    if(a == b || a == c){
+        printf("ESCALENO\n");
+    }
+    if( a != b && a !=c){
+        printf("ISÓCELES\n");
+    }
+}
