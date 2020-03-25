e73d3a25f58a7c6dde82ebe479cac7b27f39dbf2
saulo <saulo@pop-os.localdomain>
2020-03-25 11:01:17 -0300

    Alg com funções que calculão fatorial e raiz respectivamente

---
+++ algc/funcãoFateRaiz/func.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Para compilar este programa escreva no terminal  gcc -o teste teste.c -lm
+//Como a biblioteca math.h não é linkada por padrão tem que se adicionar o -lm
+int CalcFat(int n);
+float Raiz(float n, float raiz);
+
+int main(void){
+    float num, raiz, resRaiz;
+    int fat, n;
+    printf("Insira o numero do fat que você deseja:\n");
+    scanf("%d", &n);
+    fat = CalcFat(n);
+    printf("Fatorial de %d é %d\n", n, fat);
+
+    printf("Insira o número que você quer a raiz:\n");
+    scanf("%f", &num);
+    printf("Entre com o índice da raiz:\n");
+    scanf("%f", &raiz);
+    resRaiz = Raiz(num, raiz);
+    printf("A raiz de %.0f é %.0f\n", num, resRaiz);
+}
+
+int CalcFat(int n){
+    int j, fat;
+    fat = 1;
+    for(j =1; j <=n; j++){
+        fat = fat * j;
+    }
+    return fat;
+}
+
+float Raiz(float n, float raiz){
+    float res;
+    res = pow(n, (1 / raiz));
+    return res;
+}
