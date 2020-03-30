d9853aa196ee38162a7d57bdd72a9ca4081381d9
saulo <saulo@pop-os.localdomain>
2020-03-29 21:55:51 -0300

    Fatorial dos items da matriz

---
+++ algc/fatorialMatriz/fatMa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    
+    int a[5]= {1, 2, 3, 4, 5};
+    int fat = 1;
+    
+    for(int i = 0; i < 5; i++){
+        fat = fat * a[i];
+        printf(" %d ", fat);
+    }
+}
